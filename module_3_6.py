329024edeaa08c9d409398bf694de8ca452dc686
pikalov-a <apikalov1979@gmail.com>
2024-12-19 12:07:20 +0300

    Update module_3_6.py

--- module_3_6.py
+++ module_3_6.py
@@ -1,42 +1,60 @@
-###  Дополнительное практическое задание по модулю ##module_3_6.py
+###  Дополнительное практическое задание по модулю ##module_3_5
 ##Подробнее о функциях
 def calculate_structure_sum(data_structure):
     global result1
-    if isinstance(data_structure, list) or isinstance(data_structure, tuple):
-        for i in range(len(data_structure)):
-            if isinstance(data_structure, str):
-                result1 += len(data_structure)
-                return +len(data_structure)
-            elif isinstance(data_structure, int):
-                result1 += data_structure
-                return +data_structure
-            else:
-#                result1 += int(str(data_structure[0]))
-                return calculate_structure_sum(data_structure[i])
-#        return result
+#    result1=0
+    if isinstance(data_structure, list):   ###or isinstance(data_structure, tuple):
+        result=calc_list(data_structure)
     elif isinstance(data_structure, dict):
-        for key in data_structure:
-            if isinstance(data_structure, str):
-                result1 += len(data_structure)
-                return +len(data_structure)
-            elif isinstance(data_structure, int):
-                result1 += data_structure
-                return +data_structure
-            else:
-                result1 += int(str(data_structure)[0])
-                return calculate_structure_sum(dict(data_structure[key]))
-##    elif isinstance(data_structure, tuple):
+        result=calc_dict(data_structure)
+    elif isinstance(data_structure, tuple):
+        result=calc_tuple(data_structure)
+    elif isinstance(data_structure, set):
+        result=calc_set(data_structure)
     else:
-        if isinstance(data_structure, str):
-            result1 += len(data_structure)
-            return +len(data_structure)
-        elif isinstance(data_structure, int):
-            result1 += data_structure
-            return +data_structure
-        else:
-            print(type(data_structure),data_structure)
+        result = calc_sum(data_structure)
+
+def calc_list(data_structure):
+    global result1
+    for i in range(len(data_structure)):
+#        print(type(data_structure), data_structure)
+        result = calculate_structure_sum(data_structure[i])
+#        print(type(data_structure[i]), data_structure[i])
+    return result1
+
+def calc_tuple(data_structure):
+    global result1
+    for i in range(len(data_structure)):
+        result = calculate_structure_sum(data_structure[i])
+    return result1
+
+def calc_dict(data_structure):
+    global result1
+    for key in data_structure:
+        result = calc_sum(key)
+        result = calculate_structure_sum(data_structure[key])
+    return result1
+def calc_set(data_structure):
+    global result1
+    for s in data_structure:
+        result = calculate_structure_sum(s)
+    return result1
+
+def calc_sum(data_):
+    global result1
+    if isinstance(data_, str):
+        result1 += len(data_)
+#        print(result1,data_)
+        return result1+len(data_)
+    elif isinstance(data_, int):
+        result1 += data_
+#        print(result1,data_)
+        return result1+data_
+    else:
+        print('балбес',type(data_), data_)
+#        result=calculate_structure_sum(data_structure)
 result1=0
-print(result1)
+#print(result1)
 # #
 ##Входные данные (применение функции):
 data_structure = [
@@ -47,12 +65,6 @@ data_structure = [
 ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-
 result = calculate_structure_sum(data_structure)
-print(result)
+#print(result)
 print(result1)
-# print(type([1, 2, 3]))
-# print(type({'a': 4, 'b': 5}))
-# print(type((6, {'cube': 7, 'drum': 8})))
-# print(type("ghjksf"))
-# print(type(2))
